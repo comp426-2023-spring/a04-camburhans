764c0269f10086e2ded9f5467bee09f22077d6d7
Cam Burhans <cburhans@email.unc.edu>
2023-04-29 17:02:11 -0400

    some get methods for api

--- server.js
+++ server.js
@@ -11,3 +11,23 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.listen(port);
+
+app.get('/app/', (req, res) => {
+    res.status(200)
+    res.send("200 OK");
+})
+
+app.get('/app/rps/', (req, res) => {
+    res.status(200)
+    res.send(rps());
+})
+
+app.get('/app/rpsls/', (req, res) => {
+    res.status(200)
+    res.send(rpsls());
+})
+
+app.get('/app/rps/play/', (req, res) => {
+    res.status(200)
+    res.send(rps(req.query.shot))
+})
