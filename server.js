1fa05866f7badb5382c7dfaa5700e18f34842b06
Cam Burhans <cburhans@email.unc.edu>
2023-04-29 17:01:36 -0400

    constants for server.js

--- server.js
+++ server.js
@@ -3,3 +3,7 @@
 import { rps, rpsls } from "./lib/rpsls.js";
 import minimist from 'minimist';
 import express from 'express';
+
+const args = minimist(process.argv.slice(2)); 
+const port = args.port || 5000;
+const app = express();
